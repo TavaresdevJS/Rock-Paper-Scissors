0be4f28cedc9c7f45551ef35611c2eb0dd624967
Gabriel <178415368+TavaresdevJS@users.noreply.github.com>
2025-05-20 15:58:53 -0300

    feat: Add playRoud function This function is responsible for the chore mechanics of the game using If and else if statements to specify the conditions that must be met for the player to win, loose or tie against the computer.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//Function to generate random number to pick computer choice
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
     if (randomNumber === 0){
@@ -9,12 +10,58 @@ function getComputerChoice(){
     }
 }
 
+//Function to get the human choice
 function getHumanChoice(){
     const input = prompt("What’s your choice? (rock, paper, or scissors)");
     return input.toLowerCase();
 }
 
-computerChoice = getComputerChoice();
-humanChoice = getHumanChoice();
-console.log(computerChoice);
-console.log(humanChoice);
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    //IF statement to get error message if the user enters wrong input
+
+    if(
+        humanChoice !== 'rock' &&
+        humanChoice !== 'paper' &&
+        humanChoice !== 'scissors'
+    ) {
+        return "Invalid choice, stupid.";
+    }
+    
+    console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`);
+
+    if (humanChoice === computerChoice){
+        return "What!? Its a tie!!";
+    }
+
+    //All human wins possibilities
+    if (humanChoice === 'rock' && computerChoice === 'scissors'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`; 
+    } else if (humanChoice === 'rock' && computerChoice === 'paper'){ //All computer wins possibilities
+        computerScore++;
+        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+        computerScore++;
+        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+        computerScore++;
+        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
