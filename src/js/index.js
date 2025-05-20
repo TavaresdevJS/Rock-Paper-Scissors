2c0706e7bde6b1fe2e015a6a45443d536461cb10
Gabriel <178415368+TavaresdevJS@users.noreply.github.com>
2025-05-20 16:13:31 -0300

    feat: Add for and If statements for the bo5 logic Used a for statement to make a loop lgic for the 5 rounds of gameplay And used if, else if and else statements to compare socres and announce The winner

--- src/js/index.js
+++ src/js/index.js
@@ -16,52 +16,71 @@ function getHumanChoice(){
     return input.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+    let humanScore = 0;
+    let computerScore = 0;
 
-    //IF statement to get error message if the user enters wrong input
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
 
-    if(
-        humanChoice !== 'rock' &&
-        humanChoice !== 'paper' &&
-        humanChoice !== 'scissors'
-    ) {
-        return "Invalid choice, stupid.";
+        //IF statement to get error message if the user enters wrong input
+
+        if(
+            humanChoice !== 'rock' &&
+            humanChoice !== 'paper' &&
+            humanChoice !== 'scissors'
+        ) {
+            return "Invalid choice, stupid.";
+        }
+        
+        console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`);
+
+        if (humanChoice === computerChoice){
+            return "What!? Its a tie!!";
+        }
+
+        //All human wins possibilities
+        if (humanChoice === 'rock' && computerChoice === 'scissors'){
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+        } else if (humanChoice === 'paper' && computerChoice === 'rock'){
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`; 
+        } else if (humanChoice === 'rock' && computerChoice === 'paper'){ //All computer wins possibilities
+            computerScore++;
+            return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+            computerScore++;
+            return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
+            computerScore++;
+            return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+        }
+
     }
-    
-    console.log(`You chose ${humanChoice}. The computer chose ${computerChoice}.`);
 
-    if (humanChoice === computerChoice){
-        return "What!? Its a tie!!";
+    // 5 rounds gameplay loop
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        const result = playround(humanSelection, computerSelection);
+        console.log(result);
+        console.log(`Your score: ${humanScore} - Computer score: ${computerScore}`)
     }
 
-    //All human wins possibilities
-    if (humanChoice === 'rock' && computerChoice === 'scissors'){
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`;
-    } else if (humanChoice === 'paper' && computerChoice === 'rock'){
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`;
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
-        humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`; 
-    } else if (humanChoice === 'rock' && computerChoice === 'paper'){ //All computer wins possibilities
-        computerScore++;
-        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
-        computerScore++;
-        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
-        computerScore++;
-        return `Once again humanity loses to the machines! ${computerChoice} beats ${humanChoice}`;
+    //If and Else if statements to tell who is the winner
+    if (humanScore > computerScore) {
+        console.log("Congrats Playa, you won!");
+    } else if (humanScore < computerScore) {
+        console.group("The machines conquered the world. Call John Connor");
+    } else {
+        console.log("It's a tie!");
     }
-
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
+playGame();
